915bc0c26c1ff8fb80efd491e33cecbf7f2dcf26
Salvador Guzman <guzmansalv@gmail.com>
2018-04-04 22:45:48 -0700

    try simple http request

--- watch/src/com/github/sguzman/watch/Main.scala
+++ watch/src/com/github/sguzman/watch/Main.scala
@@ -1,7 +1,11 @@
 package com.github.sguzman.watch
 
 import com.thoughtworks.binding.{Binding, dom}
+import fr.hmil.roshttp.HttpRequest
+import fr.hmil.roshttp.response.SimpleHttpResponse
 import org.scalajs.dom.html.{Div, Html}
+
+import scala.util.{Failure, Success}
 
 object Main {
   @dom def body: Binding[Div] = {
@@ -32,6 +36,13 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
+    val request = HttpRequest("https://brotli-encode.herokuapp.com/sguzman/GoJS/master/store.msg.br?brotli=true")
+
+    request.send().onComplete({
+      case res:Success[SimpleHttpResponse] => println(res.get.body)
+      case _: Failure[SimpleHttpResponse] => println("Houston, we got a problem!")
+    })
+
     com.thoughtworks.binding.dom.render(org.scalajs.dom.document, render)
   }
 }
