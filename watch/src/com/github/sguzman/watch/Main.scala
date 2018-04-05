3bb20d755a8235385d18b1fb9eee5c28b5b6d316
Salvador Guzman <guzmansalv@gmail.com>
2018-04-04 21:28:35 -0700

    full html file in scala

--- watch/src/com/github/sguzman/watch/Main.scala
+++ watch/src/com/github/sguzman/watch/Main.scala
@@ -1,13 +1,21 @@
 package com.github.sguzman.watch
 
 import com.thoughtworks.binding.{Binding, dom}
-import org.scalajs.dom.html.Div
+import org.scalajs.dom.html.Html
 
 object Main {
-  @dom def render: Binding[Div] = {
-    <div>
-      <h1>Hello</h1>
-    </div>
+  @dom def render: Binding[Html] = {
+    <html>
+      <head>
+      </head>
+      <body>
+        <div id="container">
+          <header>
+            <h1>Hello</h1>
+          </header>
+        </div>
+      </body>
+    </html>
   }
 
   def main(args: Array[String]): Unit = {
